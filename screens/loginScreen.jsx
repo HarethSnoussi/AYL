6ad6ae2d9a113e79f33ef4b5655f245d7919915c
Raouf Madani <raoufmadani@iMac-de-Raouf.local>
2020-10-14 22:56:17 +0100

    set guide screen

--- screens/loginScreen.jsx
+++ screens/loginScreen.jsx
@@ -217,6 +217,7 @@ LoginScreen.navigationOptions= ()=>{
         backgroundColor: 'white'
     },
     headerBackTitle : " ",
+    headerLeft:()=>null,
     headerTitle: () => (
       <Image 
       resizeMode="cover"
